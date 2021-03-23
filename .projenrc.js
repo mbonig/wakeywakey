3061516353da862d7d4d3b51ef3af755b7b881a8
Matthew Bonig <matthew.bonig@gmail.com>
2021-03-23 08:41:23 -0600

    cdk bump to 189

--- .projenrc.js
+++ .projenrc.js
@@ -8,7 +8,7 @@ const project = new AwsCdkConstructLibrary({
   description: 'A CDK construct that will automatically start a stopped EC2 instance at a given time.',
   authorAddress: 'matthew.bonig@gmail.com',
   authorName: 'Matthew Bonig',
-  cdkVersion: '1.78.0',
+  cdkVersion: '1.89.0',
   repository: 'https://github.com/mbonig/wakeywakey',
   bin: {
     wakeywakey: 'bin/wakeywakey.js',
